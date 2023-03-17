c23299648c2e3e278aabfce04ecd9e93cb3e6cd7
sanaparvee <127944750+sanaparvee@users.noreply.github.com>
2023-03-17 17:06:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ provider "aws" {
   secret_key = ""
 }
 
-# resource "aws_s3_bucket" "bro_s3" {
-#   bucket = "bro-test-bucket"
-#   versioning {
-#     enabled = true
-#   }
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
+resource "aws_s3_bucket" "bro_s3" {
+  bucket = "bro-test-bucket"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
